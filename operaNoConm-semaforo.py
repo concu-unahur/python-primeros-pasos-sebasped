2ddb8f4f0768c0088fb4298c420f78ea41b2641a
sebasped <sebasped@disroot.org>
2020-02-12 17:53:35 -0300

    agregado oper no conmut versión semáforo

--- operaNoConm-semaforo.py
+++ operaNoConm-semaforo.py
@@ -10,21 +10,17 @@ sem = threading.Semaphore(0)
 def sumarUno():
     global cant
     global sem
-    
-    # time.sleep(1) #simula un proceso
-    
+
     try:
         cant += 1
     finally:
-       sem.release() #¿qué pasa si comento este release y descomento el pass?
-    #    pass
+       sem.release() 
 
 def multPorDos():
     global cant
     global sem
 
     sem.acquire()
-    
     try:
         cant *= 2
     finally:
@@ -34,13 +30,12 @@ def multPorDos():
 t1 = threading.Thread(target=sumarUno)
 t2 = threading.Thread(target=multPorDos)
 
-# a pesar de que lanzo primero mult por dos, se ejecuta primero
-# sumar uno, pues el semáforo lo impide.
 t2.start()
+time.sleep(1)
 t1.start()
 
 
 # y acá sigue haciendo cosas el main thread
-time.sleep(0.5)
+time.sleep(1)
 
 logging.info(f'Cantidad vale {cant}')
